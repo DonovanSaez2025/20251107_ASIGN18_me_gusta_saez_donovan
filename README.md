f80519a66fbf4058b30a58ec164d0f42ff66eddc
DonovanSaez2025 <donovansaez@liceovvh.cl>
2025-08-21 11:50:55 -0400

    corregido todo
    rellenadote

--- README.md
+++ README.md
@@ -4,6 +4,7 @@ Donovan Sáez
 **Contacto:** donovansaez@liceovvh.cl
 
 ## Descripción
+En este proyecto aprenderemos a programar la función de dar y quitar likes.
 
 ## Importante
 Si deseas apoyarme, contáctame.
---
+++ static/js/script.js
@@ -0,0 +1,20 @@
+// Función de dar like para el post número 1
+function darLike1() {
+    const spanLike1 = document.querySelector("#like1");
+    let cantidadLike1 = parseInt(spanLike1.innerText);
+    spanLike1.innerText = cantidadLike1 + 1;
+}
+
+// Función de dar like para el post número 2
+function darLike2() {
+    const spanLike2 = document.querySelector("#like2");
+    let cantidadLike2 = parseInt(spanLike2.innerText);
+    spanLike2.innerText = cantidadLike2 + 1;
+}
+
+// Función de dar like para el post número 3
+function darLike3() {
+    const spanLike3 = document.querySelector("#like3");
+    let cantidadLike3 = parseInt(spanLike3.innerText);
+    spanLike3.innerText = cantidadLike3 + 1;
+}
